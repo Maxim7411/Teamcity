4af58d1e9a6b5473e7c0ce6a5fa355d0280967b4
artx <35874101+artx52135@users.noreply.github.com>
2023-12-28 12:53:07 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -17,8 +17,8 @@
             throw new ArgumentException("Значение n должно быть положительным.");
         }
 
-        int count = 0
-        int number = 1
+        int count = 0;
+        int number = 1;
 
         while (count < n)
         {
